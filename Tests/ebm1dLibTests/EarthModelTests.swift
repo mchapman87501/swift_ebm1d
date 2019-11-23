8a2e59e3f3ad39b98d97a6cb4aabc1c6b6d9af70
Mitch Chapman <mchapman87501@gmail.com>
2019-11-23 13:34:05 -0700

    Add earth model tests.

--- Tests/ebm1dLibTests/EarthModelTests.swift
+++ Tests/ebm1dLibTests/EarthModelTests.swift
@@ -17,17 +17,45 @@ final class EarthModelTests: XCTestCase {
     // Parameterized test.
     func pTestLatsFract(_ numZones: Int) throws {
         let em = EarthModel(numZones: numZones)
-        let sum = em.latsHeight.reduce(0) { $0 + $1 }
-        XCTAssertEqual(sum, 1.0, accuracy: 0.00001)
+        let sum = em.latsFract.reduce(0) { $0 + $1 }
+        XCTAssertEqual(sum, 1.0, accuracy: 0.000001)
     }
 
-    func testLatsFract1() throws {
+    func testLatsFract() throws {
         try pTestLatsFract(9)
         try pTestLatsFract(18)
         try pTestLatsFract(36)
     }
 
+
+    func pTestLatsHeight(_ numZones: Int) throws {
+        let em = EarthModel(numZones: numZones)
+        let sum = em.latsFract.reduce(0) { $0 + $1 }
+        XCTAssertEqual(sum, 1.0, accuracy: 0.000001)
+    }
+
+    func testLatsHeight() throws {
+        try pTestLatsHeight(9)
+        try pTestLatsHeight(18)
+        try pTestLatsHeight(36)
+    }
+
+    func pTestEffectiveSolarConst(_ numZones: Int) throws {
+        let em = EarthModel(numZones: numZones)
+        let sum = em.insolByLat.reduce(0) { $0 + $1 }
+        XCTAssertEqual(sum, 1370.0 / 4.0, accuracy: 0.000001)
+    }
+
+    func testEffectiveSolarConst() throws {
+        try pTestEffectiveSolarConst(9)
+        try pTestEffectiveSolarConst(18)
+        try pTestEffectiveSolarConst(36)
+    }
+
     static var allTests = [
         ("testConstructor", testConstructor),
+        ("testLatsFract", testLatsFract),
+        ("testLatsHeight", testLatsHeight),
+        ("testEffectiveSolarConst", testEffectiveSolarConst),
     ]
 }
