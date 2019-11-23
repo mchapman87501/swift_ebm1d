578ab2fc2706aecc7a8637f4e13d69a699f4994e
Mitch Chapman <mchapman87501@gmail.com>
2019-11-23 13:21:07 -0700

    Add some earth model tests.

--- Tests/ebm1dLibTests/EarthModelTests.swift
+++ Tests/ebm1dLibTests/EarthModelTests.swift
@@ -14,6 +14,19 @@ final class EarthModelTests: XCTestCase {
         XCTAssertEqual(em.insolByLat.count, numZones)
     }
 
+    // Parameterized test.
+    func pTestLatsFract(_ numZones: Int) throws {
+        let em = EarthModel(numZones: numZones)
+        let sum = em.latsHeight.reduce(0) { $0 + $1 }
+        XCTAssertEqual(sum, 1.0, accuracy: 0.00001)
+    }
+
+    func testLatsFract1() throws {
+        try pTestLatsFract(9)
+        try pTestLatsFract(18)
+        try pTestLatsFract(36)
+    }
+
     static var allTests = [
         ("testConstructor", testConstructor),
     ]
