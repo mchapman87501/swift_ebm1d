9c9240c470e464e9f8b0d1e2c093659e81ebaf6d
Mitch Chapman <mchapman87501@gmail.com>
2019-12-12 07:36:12 -0700

    First draft: plot model solutions.

--- Tests/ebm1dLibTests/ModelTests.swift
+++ Tests/ebm1dLibTests/ModelTests.swift
@@ -4,10 +4,9 @@ import class Foundation.Bundle
 
 final class ModelTests: XCTestCase {
     func testGenTemps() throws {
-        let m = Model()
         let minSM = 0.4
         let maxSM = 15.0
-        let results = m.getSolutions(
+        let results = Model.getSolutions(
             minSM: minSM, maxSM: maxSM, gat0: -60.0, numZones: 9)
         XCTAssertTrue(results.count >= 2)
         let avgs = results.map { $0.solution.avg }
@@ -19,6 +18,24 @@ final class ModelTests: XCTestCase {
             XCTAssertLessThanOrEqual(minSM, sm)
             XCTAssertLessThanOrEqual(sm, maxSM)
         }
+    }
+
+    func testSensitivity() throws {
+        let minSM = 0.4
+        let maxSM = 15.0
+        let gat0 = -60.0
+
+        let r1 = Model.getSolutions(
+            minSM: minSM, maxSM: maxSM, gat0: gat0, numZones: 9)
+
+        let r2 = Model.getSolutions(
+            minSM: minSM, maxSM: maxSM, gat0: gat0, numZones: 90)
+
+        let diff = zip(r1, r2).map { (s1, s2) in
+            s2.solution.avg - s1.solution.avg
+        }
+        print("Solution diffs: \(diff)")
+        
     }
 
     static var allTests = [
