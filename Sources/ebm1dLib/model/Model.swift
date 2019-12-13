a023a2005e7426acb137591a620d73ddf3fb6c87
Mitch Chapman <mchapman87501@gmail.com>
2019-12-13 06:59:55 -0700

    Break out the rising from the falling solar multiplier solutions.

--- Sources/ebm1dLib/model/Model.swift
+++ Sources/ebm1dLib/model/Model.swift
@@ -4,12 +4,37 @@ public struct Model {
         public let solarMult: Double
         public let solution: TempSolver.Solution
     }
+    
+    public struct Result {
+        public let rising: [AvgTempResult]
+        public let falling: [AvgTempResult]
+    }
+
+    static func solutionSeries(
+        _ solver: TempSolver, _ delta: Double, _ smSeq: [Double],
+        _ tempsIn: [Double]
+    ) -> ([AvgTempResult], [Double]) {
+        var result = [AvgTempResult]()
+        var temps = tempsIn
+        result.reserveCapacity(smSeq.count)
+        for sm in smSeq {
+            if let solution = try? solver.solve(
+                solarMultiplier: sm, temp: temps
+            ) {
+                let record = AvgTempResult(
+                    delta: delta, solarMult: sm, solution: solution)
+                result.append(record)
+                temps = solution.temps
+            }
+        }
+        return (result, temps)
+    }
 
     public static func getSolutions(
         minSM minSolarMult: Double, maxSM maxSolarMult: Double,
         gat0 globalAvgTemp0: Double, numZones: Int,
         f latTransferCoeff: Double = Defaults.latTransferCoeff
-    ) -> [AvgTempResult] {
+    ) -> Result {
         let em = EarthModel(numZones: numZones)
         let solver = TempSolver(em: em, f: latTransferCoeff)
 
@@ -23,21 +48,10 @@ public struct Model {
         let smFalling = stride(
             from: maxSolarMult, to: minSolarMult, by: -delta).map {$0}
 
-        var result = [AvgTempResult]()
-        result.reserveCapacity(2 * numSolarMults)
-        var temps = gat0
-        for smSeq in [smRising, smFalling] {
-            for sm in smSeq {
-                if let solution = try? solver.solve(
-                    solarMultiplier: sm, temp: temps
-                ) {
-                    let record = AvgTempResult(
-                        delta: delta, solarMult: sm, solution: solution)
-                    result.append(record)
-                    temps = solution.temps
-                }
-            }
-        }
-        return result
+            
+        let temps = gat0
+        let (rRising, tempsR) = Self.solutionSeries(solver, delta, smRising, temps)
+        let (rFalling, _) = Self.solutionSeries(solver, delta, smFalling, tempsR)
+        return Result(rising: rRising, falling: rFalling)
     }
 }
--- Tests/ebm1dLibTests/ModelTests.swift
+++ Tests/ebm1dLibTests/ModelTests.swift
@@ -8,35 +8,38 @@ final class ModelTests: XCTestCase {
         let maxSM = 15.0
         let results = Model.getSolutions(
             minSM: minSM, maxSM: maxSM, gat0: -60.0, numZones: 9)
-        XCTAssertTrue(results.count >= 2)
-        let avgs = results.map { $0.solution.avg }
-        let dAvg = avgs.delta()
-        let d2AvgNormed = dAvg.delta().normed()
-        print("d2Avg: \(d2AvgNormed.formatted())")
-        for record in results {
-            let sm = record.solarMult
-            XCTAssertLessThanOrEqual(minSM, sm)
-            XCTAssertLessThanOrEqual(sm, maxSM)
-        }
-    }
-
-    func testSensitivity() throws {
-        let minSM = 0.4
-        let maxSM = 15.0
-        let gat0 = -60.0
-
-        let r1 = Model.getSolutions(
-            minSM: minSM, maxSM: maxSM, gat0: gat0, numZones: 9)
-
-        let r2 = Model.getSolutions(
-            minSM: minSM, maxSM: maxSM, gat0: gat0, numZones: 90)
-
-        let diff = zip(r1, r2).map { (s1, s2) in
-            s2.solution.avg - s1.solution.avg
-        }
-        print("Solution diffs: \(diff)")
         
+        for series in [results.rising, results.falling] {
+            XCTAssertTrue(series.count >= 2)
+            let avgs = series.map { $0.solution.avg }
+            let dAvg = avgs.delta()
+            let d2AvgNormed = dAvg.delta().normed()
+            print("d2Avg: \(d2AvgNormed.formatted())")
+            for record in series {
+                let sm = record.solarMult
+                XCTAssertLessThanOrEqual(minSM, sm)
+                XCTAssertLessThanOrEqual(sm, maxSM)
+            }
+        }
     }
+
+//    func testSensitivity() throws {
+//        let minSM = 0.4
+//        let maxSM = 15.0
+//        let gat0 = -60.0
+//
+//        let r1 = Model.getSolutions(
+//            minSM: minSM, maxSM: maxSM, gat0: gat0, numZones: 9)
+//
+//        let r2 = Model.getSolutions(
+//            minSM: minSM, maxSM: maxSM, gat0: gat0, numZones: 90)
+//
+//        let diff = zip(r1, r2).map { (s1, s2) in
+//            s2.solution.avg - s1.solution.avg
+//        }
+//        print("Solution diffs: \(diff)")
+//        
+//    }
 
     static var allTests = [
         ("testGenTemps", testGenTemps)
